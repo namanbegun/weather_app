043705c37aa6908b0518a933f086399dc4840333
namanbegun <namanbairagi65@gmail.com>
2021-08-25 17:37:01 +0530

    upload images

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int temperature = 0;
-  String location = 'San Francisco';
+  String location = 'Mumbai';
   int woeid = 2487956;
   String weather = 'clear';
   String searchApiUrl =
@@ -28,7 +28,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   initState() {
-    // TODO: implement initState
     super.initState();
     fetchlocation();
   }
